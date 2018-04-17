488703853fd0576b05fc8ac73e8e7657d83b91b3
geoffrey woodcock <theatomproject@gmail.com>
2018-04-17 17:12:32 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,34 @@
+def sort_array_asc(num)
+   num.sort do |a,b|
+     a <=> b
+end
+end
 
+def sort_array_desc(num)
+   num.sort do |a,b|
+     b <=> a
+end
+end
+
+def sort_array_char_count(str)
+    str.sort do |a, b|
+      a.length <=> b.length
+end
+end
+
+def swap_elements(array)
+  array[0], array[1], array[2] = array[0], array[2], array[1]
+end
+
+def reverse_array(num)
+  num.reverse
+  end
+  
+def kesha_maker(str)
+  dollar = []
+  str.each
+   
+end
+
+def find_a(array)
+end
